af4b00a470e99d837f46daaca3b34e8278b80a7c
shivansu77 <shivansubisht77@gmail.com>
2025-10-03 16:49:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header  text-3xl font-bold underline text-green-800">
+        Lets built netflix gpt
+      </header>
+    </div>
+  );
+}
+
+export default App;
